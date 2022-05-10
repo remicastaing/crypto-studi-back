a17d096e9f33b5a1f49b571b9b8aba2090b14a75
Rémi Castaing <remi.castaing@gmail.com>
2022-05-10 06:16:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import os
 import psycopg2
 
 on_heroku = False
-if 'YOUR_ENV_VAR' in os.environ:
+if 'ENVIRONNEMENT' in os.environ:
   on_heroku = True
 
 
-# if on_heroku:
-#   DATABASE_URL = os.environ['DATABASE_URL']
-#   conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# else:
-#   conn = psycopg2.connect(
-#     host="localhost",
-#     database="transcodb",
-#     user="postgres")
+if on_heroku:
+  DATABASE_URL = os.environ['DATABASE_URL']
+  conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+else:
+  conn = psycopg2.connect(
+    host="localhost",
+    database="transcodb",
+    user="postgres")
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,10 +25,10 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/main')
+@api.route('/environnement')
 class GetMain(Resource):
     def get(self):
-        return {'main': __name__}
+        return {'prod': on_heroku}
 
 if __name__ == '__main__':
     app.run(debug=True)
