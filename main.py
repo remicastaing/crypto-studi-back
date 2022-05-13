19c0bc08f976de8c313b63eab9c49b62900fc5b7
Rémi Castaing <remi.castaing@gmail.com>
2022-05-13 07:59:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from core.env import is_on_heroku
 app = Flask(__name__)
 api.init_app(app)
 if is_on_heroku():
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 else:
     app.run(host='0.0.0.0', port=80, debug=True)
